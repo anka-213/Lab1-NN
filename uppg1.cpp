9becd1f5312976c8afc6a75f82e7ee4ade9ebc64
Andreas Källberg <anka.213@gmail.com>
2011-09-06 19:04:54 +0200

    Automatically calculate mean over multiple randomized simulations.

--- uppg1.cpp
+++ uppg1.cpp
@@ -14,15 +14,16 @@
 
 //using namespace std;
 using namespace boost::numeric::ublas;
+	typedef boost::mt19937 RNGType;
 
-const int NEURONS = 200;
-const int PATTERNS = 30;
-const int TRIALS = 30;
+//const int NEURONS = 200;
+//const int PATTERNS = 30;
+//const int TRIALS = 30;
 const bool reflexive = false;
-boost::variate_generator<boost::rand48,boost::uniform_smallint<> > * gen;
+boost::variate_generator<RNGType,boost::uniform_smallint<> > * gen;
 
-int getWeight(int i, int j, matrix<int> &pattern);
-int doTrial();
+void doTrials(int NEURONS, int PATTERNS, int TRIALS);
+int doTrial(int NEURONS, int PATTERNS);
 
 int sign(double x) {
 	if (x > 0) {
@@ -42,26 +43,33 @@ int random_plusminus_one() {
 
 
 int main() {
-	boost::rand48 rng(std::time(0));         // produces randomness out of thin air
+	RNGType rng(std::time(0));         // produces randomness out of thin air
 					    // see pseudo-random number generators
 	boost::uniform_smallint<> zero_or_one(0,1);
 					    // distribution that maps to 1..6
 					    // see random number distributions
-	gen = new boost::variate_generator<boost::rand48,boost::uniform_smallint<> > (rng,zero_or_one);
-	int sum = 0;
-	for (int i = 0; i < TRIALS; i ++) {
-		int fail = doTrial();
-		sum += (fail == -1)? i--,0:fail; // I have found a wonderful
-						  // proof that this works but
-						  // there is not enough space
-						  // in this marigin... 
+	gen = new boost::variate_generator<RNGType,boost::uniform_smallint<> > (rng,zero_or_one);
+	
+	boost::uniform_smallint<> number_of_neurons (50, 200);
+	boost::uniform_smallint<> number_of_patterns (5, 100);
+	std::cout << "N\tp\tP_fail" << std::endl;
+	for (int i = 0; i < 1000; i++) {
+		std::cerr << "[2K" << i;
+		doTrials(number_of_neurons(rng),number_of_patterns(rng),30);
 	}
-	double mean = (double) sum/TRIALS;
-	std::cout << "P_fail = " << mean << " when N = " << NEURONS << ", p = "
-	     << PATTERNS << " and n_trials = " << TRIALS << std::endl;
 }
 
-int doTrial() {
+void doTrials(int NEURONS, int PATTERNS, int TRIALS) {
+	int sum = 0;
+	for (int i = 0; i < TRIALS; i ++) {
+		int fail = doTrial(NEURONS, PATTERNS);
+		sum += fail;
+	}
+	double mean = (double) sum/TRIALS;
+	std::cout << NEURONS << "\t" << PATTERNS << "\t" << mean << std::endl;
+}
+
+int doTrial(int NEURONS, int PATTERNS) {
 	matrix<int> weight (NEURONS, NEURONS);
 	vector<int> state (NEURONS);		 // Values -1 or 1
 	matrix<int> pattern (NEURONS, PATTERNS); // Values -1 or 1
@@ -80,7 +88,15 @@ int doTrial() {
 	
 	for (int i = 0; i < NEURONS; i++) {
 		for (int j = 0; j < NEURONS; j++) {
-			weight(i, j) = getWeight(i,j, pattern);
+			if (i == j && ! reflexive) {
+				weight(i, j) = 0;
+			} else {
+				int sum = 0;
+				for (int mu = 0; mu < PATTERNS; mu++) {
+					sum += pattern(i, mu) * pattern(j, mu);
+				}
+				weight(i, j) = sum;
+			}
 		//	std::cout << "Weight[" << i << "][" << j << "] = " << (float) weight[i][j]/PATTERNS << std::endl;
 		}
 	}
@@ -111,14 +127,3 @@ int doTrial() {
 
 // weight[i][i] = 0;
 // weight[i][j] = 1.0/NEURONS * (sum over PATTERNS mu (pattern[i][mu] * pattern[j][mu]));
-int getWeight(int i, int j, matrix<int> &pattern) {
-	if (i == j && ! reflexive) {
-		return 0;
-	} else {
-		int sum = 0;
-		for (int mu = 0; mu < PATTERNS; mu++) {
-			sum += pattern(i, mu) * pattern(j, mu);
-		}
-		return sum;
-	}
-}
